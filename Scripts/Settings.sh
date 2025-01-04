67acdf6201608fe48323083a2c4be77ddba3425a
openwrtTee <165190199+openwrtTee@users.noreply.github.com>
2025-01-04 22:18:46 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -47,10 +47,13 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 	#取消nss相关feed
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
+	#设置NSS版本
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
+	echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
+fi
 
-	if [[ $WRT_TARGET == *"60XX"* ]]; then
-		#设置NSS版本
-		echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
-	fi
+#编译器优化
+if [[ $WRT_TARGET != *"X86"* ]]; then
+	echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
+	echo "CONFIG_TARGET_OPTIMIZATION=\"-O3 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
 fi
