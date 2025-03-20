f41704bc7df07024c8b20f3b8783ee1c17514176
openwrtTee <165190199+openwrtTee@users.noreply.github.com>
2025-03-20 20:59:45 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -26,8 +26,8 @@ fi
 if [ -d *"luci-theme-argon"* ]; then
 	cd ./luci-theme-argon/
 
-	sed -i '/font-weight:/ {/!important/! s/\(font-weight:\s*\)[^;]*;/\1normal;/}' $(find ./luci-theme-argon -type f -iname "*.css")
-	sed -i "s/primary '.*'/primary '#69d7cd'/; s/'0.2'/'0.5'/; s/'none'/'bing'/" ./luci-app-argon-config/root/etc/config/argon
+	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
+	sed -i "s/primary '.*'/primary '#69d7cd'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
 
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
@@ -35,7 +35,7 @@ fi
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
 if [ -f "$NSS_DRV" ]; then
-	sed -i 's/START=.*/START=85/g' "$qca_drv_path" $NSS_DRV
+	sed -i 's/START=.*/START=85/g' $NSS_DRV
 
 	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
 fi
@@ -43,7 +43,7 @@ fi
 #修改qca-nss-pbuf启动顺序
 NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
 if [ -f "$NSS_PBUF" ]; then
-	sed -i 's/START=.*/START=86/g' "$qca_drv_path" $NSS_PBUF
+	sed -i 's/START=.*/START=86/g' $NSS_PBUF
 
 	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
 fi
